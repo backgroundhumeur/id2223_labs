a8f0e16bc48519f5159eec207eb678056d52fa43
humeur <antopaopa@gmail.com>
2022-12-09 21:48:36 +0100

    Added the code for the hf spaces

---
+++ lab2/huggingface-space-Swedish-Whisper-from-Youtube/README.md
@@ -0,0 +1,12 @@
+---
+title: Swedish Whisper From Youtube
+emoji: 📊
+colorFrom: green
+colorTo: purple
+sdk: gradio
+sdk_version: 3.12.0
+app_file: app.py
+pinned: false
+---
+
+Check out the configuration reference at https://huggingface.co/docs/hub/spaces-config-reference
---
+++ lab2/huggingface-space-Swedish-Whisper-from-Youtube/app.py
@@ -0,0 +1,51 @@
+import gradio as gr
+from pytube import YouTube
+from transformers import pipeline
+
+class GradioInference():
+  def __init__(self):
+    self.model = pipeline(model="humeur/lab2_id2223")
+    self.yt = None
+  
+  def __call__(self, link):
+    if self.yt is None:
+      self.yt = YouTube(link)
+    path = self.yt.streams.filter(only_audio=True)[0].download(filename="tmp.mp4")
+
+    results = self.model(path)
+    return results["text"]
+
+  def populate_metadata(self, link):
+    self.yt = YouTube(link)
+    return self.yt.thumbnail_url, self.yt.title
+
+gio = GradioInference()
+title="SWED->EN Youtube Transcriber (Whisper)"
+description="Speech to text transcription of Youtube videos using OpenAI's Whisper finetunned for Swedish to English translation"
+
+block = gr.Blocks()
+with block:
+    gr.HTML(
+        f"""
+            <div style="text-align: center; max-width: 500px; margin: 0 auto;">
+              <div>
+                <h1>{title}</h1>
+              </div>
+              <p style="margin-bottom: 10px; font-size: 94%">
+                {description}
+              </p>
+            </div>
+        """
+    )
+    with gr.Group():
+        with gr.Box():
+          link = gr.Textbox(label="YouTube Link")
+          title = gr.Label(label="Video Title")
+          with gr.Row().style(equal_height=True):
+            img = gr.Image(label="Thumbnail")
+            text = gr.Textbox(label="Transcription", placeholder="Transcription Output", lines=10)
+          with gr.Row().style(equal_height=True):
+              btn = gr.Button("Transcribe")
+          btn.click(gio, inputs=[link], outputs=[text])
+          link.change(gio.populate_metadata, inputs=[link], outputs=[img, title])
+block.launch()
